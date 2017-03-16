353d51cc078a301a7c6ab5964101f8b963a4057a
yambal <jy@yambal.net>
2017-03-16 14:03:34 +0900

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,52 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        copy: {
+            html: {
+                files: [
+                    { expand: true, cwd: 'src/html/', src: ['index.html'], dest: 'dist/', filter: 'isFile' }
+                ]
+            },
+            js: {
+                files: [
+                    { expand: true, cwd: 'src/js/', src: ['QTools.js'], dest: 'dist/js/', filter: 'isFile' }
+                ]
+            }
+        },
+        less:{
+            dist: {
+                options: {
+                    compress: false
+                },
+                files: [
+                    {"dist/css/style.css": "src/css/style.less"},
+                ]
+            }
+        },
+        babel: {
+            options: {
+                plugins: ['transform-react-jsx'],
+                presets: ['es2015', 'react']
+            },
+            app: {
+                files: [
+                    { 'src/js/react/AppEntry.js': 'src/js/react/jsx/AppEntry.jsx' },
+                    { 'src/js/react/QTools.js': 'src/js/react/jsx/QTools.jsx' }
+                ]
+            }
+        },
+        browserify: {
+            app: {
+                files: {
+                    'src/js/QTools.js': ['src/js/react/AppEntry.js']
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks("grunt-contrib-less");
+    grunt.loadNpmTasks('grunt-babel');
+    grunt.loadNpmTasks('grunt-browserify');
+
+    grunt.registerTask('_Dev', ['copy:html', 'less', 'babel', 'browserify', 'copy:js']);
+};
---
+++ src/js/react/jsx/AppEntry.jsx
@@ -0,0 +1,46 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var QTools = require('./QTools.js');
+
+
+(function($) {
+
+	// cordova 判定
+	/*
+	if(typeof cordova !== "undefined"){
+		console.log(cordova);
+	}
+	*/
+	var isMonaca = false;
+	var isDeviceready = false;
+	var isFiredRender = false;
+
+	if(typeof monaca !== "undefined"){
+		isMonaca = true;
+		document.addEventListener('deviceready', function(){
+			isDeviceready = true;
+			onDeviceReady();
+		}, false);
+	}
+	$(document).ready(function() {
+    	onDeviceReady();
+	});
+	
+	function onDeviceReady(){
+		if(isMonaca && isDeviceready && !isFiredRender){
+			fireRender();
+			return;
+		}else if(!isFiredRender){
+			fireRender();
+		}
+	}
+
+	function fireRender(){
+		isFiredRender = true;
+		ReactDOM.render(
+			<QTools>Entry</QTools>,
+			document.getElementById('App')
+		);
+	};
+
+}(window.jQuery));
